db66ddfa24e95ad57b5e672da1366e12439b0202
Marco Sinigaglia <marco.sinigaglia@innove.it>
2016-05-23 00:38:13 +0200

    Change functions to use Promise

--- BleManager.ios.js
+++
@@ -1,9 +0,0 @@
-/**
- * @providesModule BleManager
- * @flow
- */
-'use strict';
-var React = require('react-native');
-var BleManager = React.NativeModules.BleManager;
-
-module.exports = BleManager;
---
+++ BleManager.js
@@ -0,0 +1,61 @@
+'use strict';
+var React = require('react-native');
+var bleManager = React.NativeModules.BleManager;
+
+class BleManager  {
+
+  write(peripheralId, serviceUUID, characteristicUUID, data) {
+    return new Promise((fulfill, reject) => {
+      bleManager.write(peripheralId, serviceUUID, characteristicUUID, data, (success) => {
+        fulfill();
+      }, (fail) => {
+        reject(fail);
+      });
+    });
+  }
+
+  connect(peripheralId) {
+    return new Promise((fulfill, reject) => {
+      bleManager.connect(peripheralId,(success) => {
+        fulfill();
+      }, (fail) => {
+        reject(fail);
+      });
+    });
+  }
+
+  disconnect(peripheralId) {
+    return new Promise((fulfill, reject) => {
+      bleManager.disconnect(peripheralId,(success) => {
+        fulfill();
+      }, (fail) => {
+        reject(fail);
+      });
+    });
+  }
+
+  startNotification(peripheralId, serviceUUID, characteristicUUID) {
+    return new Promise((fulfill, reject) => {
+      bleManager.startNotification(peripheralId, serviceUUID, characteristicUUID, (success) => {
+        fulfill();
+      }, (fail) => {
+        reject(fail);
+      });
+    });
+  }
+
+  checkState() {
+    bleManager.checkState();
+  }
+
+  scan(serviceUUIDs, seconds) {
+    return new Promise((fulfill, reject) => {
+      bleManager.scan(serviceUUIDs, seconds,(success) => {
+        fulfill();
+      });
+    });
+  }
+
+}
+
+module.exports = new BleManager();
