a24987be6fa83f7078e90e2ba68e0d7722a4d6b5
Mathieu Robidoux <mrobidoux@pbsc.com>
2018-08-27 10:23:11 -0400

    added error code

--- ios/BleManager.m
+++ ios/BleManager.m
@@ -676,7 +676,7 @@ RCT_EXPORT_METHOD(requestMTU:(NSString *)deviceUUID mtu:(NSInteger)mtu callback:
         if (error) {
             NSLog(@"%@", error);
             [writeCallbacks removeObjectForKey:key];
-            writeCallback(@[error.localizedDescription]);
+            writeCallback(@[[NSString stringWithFormat:@"%@ - status: %d", error.localizedDescription, error.code]);
         } else {
             if ([writeQueue count] == 0) {
                 [writeCallbacks removeObjectForKey:key];
