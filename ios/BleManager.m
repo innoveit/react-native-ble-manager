6ebeeb40e6bda6491795139cb802e789f62b7481
henghonglee <henghong.lee@gmail.com>
2019-01-09 11:50:08 +0800

    Add missing synchronized calls to correctly fix threading issues for NSMutableSet peripherals

--- ios/BleManager.m
+++ ios/BleManager.m
@@ -60,12 +60,13 @@ bool hasListeners;
         
         manager.delegate = nil;
     }
+    @synchronized(peripherals) {
+        for (CBPeripheral* p in peripherals) {
+            p.delegate = nil;
+        }
     
-    for (CBPeripheral* p in peripherals) {
-        p.delegate = nil;
+        peripherals = [NSMutableSet set];
     }
-    
-    peripherals = [NSMutableSet set];
 }
 
 +(BOOL)requiresMainQueueSetup
@@ -254,10 +255,12 @@ RCT_EXPORT_METHOD(getConnectedPeripherals:(NSArray *)serviceUUIDStrings callback
 
     NSMutableArray *foundedPeripherals = [NSMutableArray array];
     if ([serviceUUIDs count] == 0){
-        for(CBPeripheral *peripheral in peripherals){
-            if([peripheral state] == CBPeripheralStateConnected){
-                NSDictionary * obj = [peripheral asDictionary];
-                [foundedPeripherals addObject:obj];
+        @synchronized(peripherals) {
+            for(CBPeripheral *peripheral in peripherals){
+                if([peripheral state] == CBPeripheralStateConnected){
+                    NSDictionary * obj = [peripheral asDictionary];
+                    [foundedPeripherals addObject:obj];
+                }
             }
         }
     } else {
