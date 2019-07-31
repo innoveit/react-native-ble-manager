f6a421c9493145976f432dedd0d60ec6b886717a
Nikhil Gupta <nikhilgupta240@gmail.com>
2019-07-31 10:41:40 +0530

    Fixing Package Manager import

--- android/src/main/java/it/innove/BleManager.java
+++ android/src/main/java/it/innove/BleManager.java
@@ -9,6 +9,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.Log;
