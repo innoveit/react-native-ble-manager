eb30d1440c0697fe2aa8a45d5a368c80cd979ced
Marco Sinigaglia <marco.sinigaglia@innove.it>
2019-09-27 17:26:49 +0200

    Fix missing serviceData

--- android/src/main/java/it/innove/LollipopPeripheral.java
+++ android/src/main/java/it/innove/LollipopPeripheral.java
@@ -59,6 +59,7 @@ public class LollipopPeripheral extends Peripheral {
 					}
 				}
 
+                advertising.putMap("serviceData", serviceData);
 				advertising.putInt("txPowerLevel", advertisingData.getTxPowerLevel());
 			}
 
