d3edb15047399f534bef0949e402bc1898ab1b2a
David Dolheguy <ddolheguy@gmail.com>
2019-12-26 10:47:49 +0000

    Fixed duplicate line

--- ios/CBPeripheral+Extensions.m
+++ ios/CBPeripheral+Extensions.m
@@ -111,7 +111,6 @@ static char ADVERTISEMENT_RSSI_IDENTIFER;
     NSLog(@"%@", serviceData);
     
     for (CBUUID *key in [serviceData allKeys]) {
-        [serviceData setObject:dataToArrayBuffer([serviceData objectForKey:key]) forKey:[key UUIDString]];
         if ([serviceData objectForKey:key]) {
             [serviceData setObject:dataToArrayBuffer([serviceData objectForKey:key]) forKey:[key UUIDString]];
             [serviceData removeObjectForKey:key];
