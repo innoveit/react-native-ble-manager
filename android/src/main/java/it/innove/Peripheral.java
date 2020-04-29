0dd4ff624c0d36ee2cd44c3615a4900f7f63f285
rob lingstuyl <808numbers@gmail.com>
2020-04-29 17:53:48 -0600

    formating only

--- android/src/main/java/it/innove/Peripheral.java
+++ android/src/main/java/it/innove/Peripheral.java
@@ -277,10 +277,9 @@ public class Peripheral extends BluetoothGattCallback {
 				@Override
 				public void run() {
 					try {
-						this.gatt.discoverServices();
+						gatt.discoverServices();
 					} catch (NullPointerException e) {
-					Log.d(BleManager.LOG_TAG, "onConnectionStateChange connected but gatt of Run
-					method was null");
+						Log.d(BleManager.LOG_TAG, "onConnectionStateChange connected but gatt of Run method was null");
 					}
 				}
 			});
