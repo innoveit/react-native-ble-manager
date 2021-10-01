3ae66d3795aef1b423ff5cc224e5292c29275472
marco.sinigaglia@innove.it <sinigaglia.marco@gmail.com>
2021-10-01 17:28:58 +0200

    Fix example on iOS

--- example/App.js
+++ example/App.js
@@ -29,7 +29,7 @@ import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
-import BleManager from 'react-native-ble-manager';
+import BleManager from '../BleManager';
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
--- example/metro.config.js
+++ example/metro.config.js
@@ -5,6 +5,15 @@
  * @format
  */
 
+ const path = require('path');
+ const extraNodeModules = {
+   'common': path.resolve(__dirname + '/../'),
+ };
+ const watchFolders = [
+   path.resolve(__dirname + '/../')
+ ];
+ 
+
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
@@ -14,4 +23,12 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        //redirects dependencies referenced from common/ to local node_modules
+        name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
+    }),
+  },
+  watchFolders,
 };
