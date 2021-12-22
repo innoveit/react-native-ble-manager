33d27844c94a5fdc575174545d0bbc780ea0f78e
Chester Kwan <chester@zunosaki.com>
2021-12-22 10:17:22 +0800

    fix peripheral not found while scanning

--- android/src/main/java/it/innove/BleManager.java
+++ android/src/main/java/it/innove/BleManager.java
@@ -193,7 +193,7 @@ class BleManager extends ReactContextBaseJavaModule {
             for (Iterator<Map.Entry<String, Peripheral>> iterator = peripherals.entrySet().iterator(); iterator
                     .hasNext(); ) {
                 Map.Entry<String, Peripheral> entry = iterator.next();
-                if (!entry.getValue().isConnected()) {
+                if (!(entry.getValue().isConnected() || entry.getValue().isConnecting())) {
                     iterator.remove();
                 }
             }
