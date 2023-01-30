676b51e43b92b4c64c34d5c519f1783af1b63cf0
thomasvo <thomasttvo@gmail.com>
2023-01-30 15:03:33 -0800

    fix indentation

--- android/src/main/java/it/innove/LollipopScanManager.java
+++ android/src/main/java/it/innove/LollipopScanManager.java
@@ -32,142 +32,142 @@ import java.util.List;
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 public class LollipopScanManager extends ScanManager {
 
-  public LollipopScanManager(ReactApplicationContext reactContext, BleManager bleManager) {
-    super(reactContext, bleManager);
-  }
-
-  @Override
-  public void stopScan(Callback callback) {
-    // update scanSessionId to prevent stopping next scan by running timeout thread
-    scanSessionId.incrementAndGet();
-
-    getBluetoothAdapter().getBluetoothLeScanner().stopScan(mScanCallback);
-    callback.invoke();
-  }
-
-  @Override
-  public void scan(ReadableArray serviceUUIDs, final int scanSeconds, ReadableMap options, Callback callback) {
-    ScanSettings.Builder scanSettingsBuilder = new ScanSettings.Builder();
-    List<ScanFilter> filters = new ArrayList<>();
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && options.hasKey("legacy")) {
-      scanSettingsBuilder.setLegacy(options.getBoolean("legacy"));
+    public LollipopScanManager(ReactApplicationContext reactContext, BleManager bleManager) {
+        super(reactContext, bleManager);
     }
 
-    if (options.hasKey("scanMode")) {
-      scanSettingsBuilder.setScanMode(options.getInt("scanMode"));
+    @Override
+    public void stopScan(Callback callback) {
+        // update scanSessionId to prevent stopping next scan by running timeout thread
+        scanSessionId.incrementAndGet();
+
+        getBluetoothAdapter().getBluetoothLeScanner().stopScan(mScanCallback);
+        callback.invoke();
     }
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      if (options.hasKey("numberOfMatches")) {
-        scanSettingsBuilder.setNumOfMatches(options.getInt("numberOfMatches"));
-      }
-      if (options.hasKey("matchMode")) {
-        scanSettingsBuilder.setMatchMode(options.getInt("matchMode"));
-      }
-    }
+    @Override
+    public void scan(ReadableArray serviceUUIDs, final int scanSeconds, ReadableMap options, Callback callback) {
+        ScanSettings.Builder scanSettingsBuilder = new ScanSettings.Builder();
+        List<ScanFilter> filters = new ArrayList<>();
 
-    if (options.hasKey("reportDelay")) {
-      scanSettingsBuilder.setReportDelay(options.getInt("reportDelay"));
-    }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && options.hasKey("legacy")) {
+            scanSettingsBuilder.setLegacy(options.getBoolean("legacy"));
+        }
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && options.hasKey("phy")) {
-      int phy = options.getInt("phy");
-      if (phy == BluetoothDevice.PHY_LE_CODED && getBluetoothAdapter().isLeCodedPhySupported()) {
-        scanSettingsBuilder.setPhy(BluetoothDevice.PHY_LE_CODED);
-      }
-      if (phy == BluetoothDevice.PHY_LE_2M && getBluetoothAdapter().isLe2MPhySupported()) {
-        scanSettingsBuilder.setPhy(BluetoothDevice.PHY_LE_2M);
-      }
-    }
+        if (options.hasKey("scanMode")) {
+            scanSettingsBuilder.setScanMode(options.getInt("scanMode"));
+        }
 
-    if (serviceUUIDs.size() > 0) {
-      for (int i = 0; i < serviceUUIDs.size(); i++) {
-        ScanFilter filter = new ScanFilter.Builder().setServiceUuid(new ParcelUuid(UUIDHelper.uuidFromString(serviceUUIDs.getString(i)))).build();
-        filters.add(filter);
-        Log.d(bleManager.LOG_TAG, "Filter service: " + serviceUUIDs.getString(i));
-      }
-    }
-
-    getBluetoothAdapter().getBluetoothLeScanner().startScan(filters, scanSettingsBuilder.build(), mScanCallback);
-    if (scanSeconds > 0) {
-      Thread thread = new Thread() {
-        private int currentScanSession = scanSessionId.incrementAndGet();
-
-        @Override
-        public void run() {
-
-          try {
-            Thread.sleep(scanSeconds * 1000);
-          } catch (InterruptedException ignored) {
-          }
-
-          runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-              BluetoothAdapter btAdapter = getBluetoothAdapter();
-              // check current scan session was not stopped
-              if (scanSessionId.intValue() == currentScanSession) {
-                if (btAdapter.getState() == BluetoothAdapter.STATE_ON) {
-                  btAdapter.getBluetoothLeScanner().stopScan(mScanCallback);
-                }
-                WritableMap map = Arguments.createMap();
-                map.putInt("status", 10);
-                bleManager.sendEvent("BleManagerStopScan", map);
-              }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (options.hasKey("numberOfMatches")) {
+                scanSettingsBuilder.setNumOfMatches(options.getInt("numberOfMatches"));
             }
-          });
-
+            if (options.hasKey("matchMode")) {
+                scanSettingsBuilder.setMatchMode(options.getInt("matchMode"));
+            }
         }
 
-      };
-      thread.start();
+        if (options.hasKey("reportDelay")) {
+            scanSettingsBuilder.setReportDelay(options.getInt("reportDelay"));
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && options.hasKey("phy")) {
+            int phy = options.getInt("phy");
+            if (phy == BluetoothDevice.PHY_LE_CODED && getBluetoothAdapter().isLeCodedPhySupported()) {
+                scanSettingsBuilder.setPhy(BluetoothDevice.PHY_LE_CODED);
+            }
+            if (phy == BluetoothDevice.PHY_LE_2M && getBluetoothAdapter().isLe2MPhySupported()) {
+                scanSettingsBuilder.setPhy(BluetoothDevice.PHY_LE_2M);
+            }
+        }
+
+        if (serviceUUIDs.size() > 0) {
+            for (int i = 0; i < serviceUUIDs.size(); i++) {
+                ScanFilter filter = new ScanFilter.Builder().setServiceUuid(new ParcelUuid(UUIDHelper.uuidFromString(serviceUUIDs.getString(i)))).build();
+                filters.add(filter);
+                Log.d(bleManager.LOG_TAG, "Filter service: " + serviceUUIDs.getString(i));
+            }
+        }
+
+        getBluetoothAdapter().getBluetoothLeScanner().startScan(filters, scanSettingsBuilder.build(), mScanCallback);
+        if (scanSeconds > 0) {
+            Thread thread = new Thread() {
+                private int currentScanSession = scanSessionId.incrementAndGet();
+
+                @Override
+                public void run() {
+
+                    try {
+                        Thread.sleep(scanSeconds * 1000);
+                    } catch (InterruptedException ignored) {
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            BluetoothAdapter btAdapter = getBluetoothAdapter();
+                            // check current scan session was not stopped
+                            if (scanSessionId.intValue() == currentScanSession) {
+                                if (btAdapter.getState() == BluetoothAdapter.STATE_ON) {
+                                    btAdapter.getBluetoothLeScanner().stopScan(mScanCallback);
+                                }
+                                WritableMap map = Arguments.createMap();
+                                map.putInt("status", 10);
+                                bleManager.sendEvent("BleManagerStopScan", map);
+                            }
+                        }
+                    });
+
+                }
+
+            };
+            thread.start();
+        }
+        callback.invoke();
     }
-    callback.invoke();
-  }
 
-  private ScanCallback mScanCallback = new ScanCallback() {
-    @Override
-    public void onScanResult(final int callbackType, final ScanResult result) {
-
-      runOnUiThread(new Runnable() {
+    private ScanCallback mScanCallback = new ScanCallback() {
         @Override
-        public void run() {
-          String info;
-          ScanRecord record = result.getScanRecord();
-          if (record != null)
-            info = record.getDeviceName();
-          else if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED)
-            info = result.getDevice().getName();
-          else
-            info = result.toString();
+        public void onScanResult(final int callbackType, final ScanResult result) {
 
-          Log.i(bleManager.LOG_TAG, "DiscoverPeripheral: " + info);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String info;
+                    ScanRecord record = result.getScanRecord();
+                    if (record != null)
+                        info = record.getDeviceName();
+                    else if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED)
+                        info = result.getDevice().getName();
+                    else
+                        info = result.toString();
 
-          LollipopPeripheral peripheral = (LollipopPeripheral) bleManager.getPeripheral(result.getDevice());
-          if (peripheral == null) {
-            peripheral = new LollipopPeripheral(bleManager.getReactContext(), result);
-          } else {
-            peripheral.updateData(result);
-            peripheral.updateRssi(result.getRssi());
-          }
-          bleManager.savePeripheral(peripheral);
+                    Log.i(bleManager.LOG_TAG, "DiscoverPeripheral: " + info);
 
-          WritableMap map = peripheral.asWritableMap();
-          bleManager.sendEvent("BleManagerDiscoverPeripheral", map);
+                    LollipopPeripheral peripheral = (LollipopPeripheral) bleManager.getPeripheral(result.getDevice());
+                    if (peripheral == null) {
+                        peripheral = new LollipopPeripheral(bleManager.getReactContext(), result);
+                    } else {
+                        peripheral.updateData(result);
+                        peripheral.updateRssi(result.getRssi());
+                    }
+                    bleManager.savePeripheral(peripheral);
+
+                    WritableMap map = peripheral.asWritableMap();
+                    bleManager.sendEvent("BleManagerDiscoverPeripheral", map);
+                }
+            });
         }
-      });
-    }
 
-    @Override
-    public void onBatchScanResults(final List<ScanResult> results) {
-    }
+        @Override
+        public void onBatchScanResults(final List<ScanResult> results) {
+        }
 
-    @Override
-    public void onScanFailed(final int errorCode) {
-      WritableMap map = Arguments.createMap();
-      map.putInt("status", errorCode);
-      bleManager.sendEvent("BleManagerStopScan", map);
-    }
-  };
+        @Override
+        public void onScanFailed(final int errorCode) {
+            WritableMap map = Arguments.createMap();
+            map.putInt("status", errorCode);
+            bleManager.sendEvent("BleManagerStopScan", map);
+        }
+    };
 }
