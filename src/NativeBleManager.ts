e26359a6e1bf0409136b27c1e8de4754e99a34b7
Lucas Alves <lucas.developing@gmail.com>
2024-10-29 11:18:51 -0300

    Add initial codegen typescript codegen support.

--- src/NativeBleManager.ts
+++ src/NativeBleManager.ts
@@ -1,42 +1,42 @@
 import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
 import { TurboModuleRegistry } from 'react-native';
-import {UnsafeObject} from "react-native/Libraries/Types/CodegenTypes";
+import {BleState, ConnectOptions, Peripheral, PeripheralInfo, ScanOptions, StartOptions} from "./types";
 
 export interface Spec extends TurboModule {
-    start(options: UnsafeObject, callback: (response: object) => void): void;
+    start(options: StartOptions, callback: (error: string|null) => void): void;
 
     scan(
         serviceUUIDStrings: string[],
         timeoutSeconds: number,
         allowDuplicates: boolean,
-        scanningOptions: UnsafeObject,
-        callback: (response: object) => void
+        scanningOptions: ScanOptions,
+        callback: (error: string|null) => void
     ): void;
 
 
-    stopScan(callback: (response: object) => void): void;
+    stopScan(callback: (error: string|null) => void): void;
 
     connect(
         peripheralUUID: string,
-        options: UnsafeObject,
-        callback: (response: object) => void
+        options: ConnectOptions,
+        callback: (error: string|null) => void
     ): void;
 
     disconnect(
         peripheralUUID: string,
         force: boolean,
-        callback: (response: object) => void
+        callback: (error: string|null) => void
     ): void;
 
     retrieveServices(
         peripheralUUID: string,
         services: string[],
-        callback: (response: object) => void
+        callback: (error: string|null, peripheral: PeripheralInfo) => void
     ): void;
 
     readRSSI(
         peripheralUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null, rssi: number) => void
     ): void;
 
     readDescriptor(
@@ -44,7 +44,7 @@ export interface Spec extends TurboModule {
         serviceUUID: string,
         characteristicUUID: string,
         descriptorUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null, data: number[]) => void
     ): void;
 
     writeDescriptor(
@@ -52,13 +52,13 @@ export interface Spec extends TurboModule {
         serviceUUID: string,
         characteristicUUID: string,
         descriptorUUID: string,
-        message: object[],
-        callback: (response: object) => void
+        data: object[],
+        callback: (error: string | null) => void
     ): void;
 
-    getDiscoveredPeripherals(callback: (response: object) => void): void;
+    getDiscoveredPeripherals(callback: (error: string | null, result: Peripheral[] | null) => void): void;
 
-    checkState(callback: (response: object) => void): void;
+    checkState(callback: (state: BleState) => void): void;
 
     write(
         peripheralUUID: string,
@@ -66,7 +66,7 @@ export interface Spec extends TurboModule {
         characteristicUUID: string,
         message: object[],
         maxByteSize: number,
-        callback: (response: object) => void
+        callback: (error: string | null) => void
     ): void;
 
     writeWithoutResponse(
@@ -76,106 +76,105 @@ export interface Spec extends TurboModule {
         message: object[],
         maxByteSize: number,
         queueSleepTime: number,
-        callback: (response: object) => void
+        callback: (error: string | null) => void
     ): void;
 
     read(
         peripheralUUID: string,
         serviceUUID: string,
         characteristicUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null, data: number[]) => void
     ): void;
 
     startNotification(
         peripheralUUID: string,
         serviceUUID: string,
         characteristicUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null) => void
     ): void;
 
     stopNotification(
         peripheralUUID: string,
         serviceUUID: string,
         characteristicUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null) => void
     ): void;
 
     getConnectedPeripherals(
         serviceUUIDStrings: string[],
-        callback: (response: object) => void
+        callback: (error: string | null, result: Peripheral[] | null) => void
     ): void;
 
     isPeripheralConnected(
         peripheralUUID: string,
-        callback: (response: object) => void
+        callback: (error: Peripheral[]) => void
     ): void;
 
-    isScanning(callback: (response: object) => void): void;
+    isScanning(callback: (error: string | null, status: boolean) => void): void;
 
     getMaximumWriteValueLengthForWithoutResponse(
         peripheralUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null, max: number) => void
     ): void;
 
     getMaximumWriteValueLengthForWithResponse(
         deviceUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null, max: number) => void
     ): void;
 
-    enableBluetooth(callback: (response: object) => void): void;
+    enableBluetooth(callback: (error: string | null) => void): void;
 
-    getBondedPeripherals(callback: (response: object) => void): void;
+    getBondedPeripherals(callback: (error: string | null, result: Peripheral[] | null) => void): void;
 
     createBond(
         peripheralUUID: string,
         devicePin: string,
-        callback: (response: object) => void
+        callback: (error: string | null) => void
     ): void;
 
     removeBond(
         peripheralUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null) => void
     ): void;
 
     removePeripheral(
         peripheralUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null) => void
     ): void;
 
     requestMTU(
         peripheralUUID: string,
         mtu: number,
-        callback: (response: object) => void
+        callback: (error: string | null, mtu: number) => void
     ): void;
 
     requestConnectionPriority(
         peripheralUUID: string,
         connectionPriority: number,
-        callback: (response: object) => void
+        callback: (error: string | null, status: boolean) => void
     ): void;
 
     refreshCache(
         peripheralUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null, result: boolean) => void
     ): void;
 
     setName(
-        name: string,
-        callback: (response: object) => void
+        name: string
     ): void;
 
-    getAssociatedPeripherals(callback: (response: object) => void): void;
+    getAssociatedPeripherals(callback: (error: string | null, peripherals: Peripheral[] | null) => void): void;
 
     removeAssociatedPeripheral(
         peripheralUUID: string,
-        callback: (response: object) => void
+        callback: (error: string | null) => void
     ): void;
 
-    supportsCompanion(callback: (response: object) => void): void;
+    supportsCompanion(callback: (supports: boolean) => void): void;
 
     companionScan(
         serviceUUIDs: string[],
-        callback: (response: object) => void
+        callback: (error: string | null, peripheral: Peripheral | null) => void
     ): void;
 }
 
