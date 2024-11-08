5366ed93340c1cae63466625475dc72680f67490
Marco Sinigaglia <marco.sinigaglia@innove.it>
2024-11-08 16:37:21 +0100

    Updated expo with new arch

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,33 +1,5 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const { getDefaultConfig } = require('expo/metro-config');
 
-const path = require('path');
-const extraNodeModules = {
-  common: path.join(__dirname, '/../'),
-};
-const watchFolders = [path.join(__dirname, '/../')];
+const config = getDefaultConfig(__dirname);
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    extraNodeModules: new Proxy(extraNodeModules, {
-      get: (target, name) =>
-        //redirects dependencies referenced from common/ to local node_modules
-        name in target
-          ? target[name]
-          : path.join(process.cwd(), `node_modules/${name}`),
-    }),
-  },
-  watchFolders,
-};
+module.exports = config;
