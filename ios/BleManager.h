6891f8e570e62dea74da4bca65bdc4022622f61b
Lucas Alves <lucas.developing@gmail.com>
2024-11-13 16:02:12 -0300

    Tried to fix swift/objective c wrapper.

--- ios/BleManager.h
+++ ios/BleManager.h
@@ -1,5 +1,6 @@
 #import <CoreBluetooth/CoreBluetooth.h>
 #import "React/RCTEventEmitter.h"
+#import "React/RCTEventDispatcher.h"
 
 /**
 This strategy assumes that all apps are new arch by default as described on docs of 0.76 release.
@@ -7,16 +8,10 @@ And enables old bridge just if user sets RCT_NEW_ARCH_ENABLED=0 on env.
 */
 
 #ifdef RCT_NEW_ARCH_ENABLED
-
 #import "RNBleManagerSpec.h"
-@interface BleManager: NSObject <NativeBleManagerSpec>
-@end
 
 #else
-
 #import <React/RCTBridgeModule.h>
-@interface BleManager: NSObject <RCTBridgeModule>
-@end
 
 #endif
 
--- ios/BleManager.mm
+++ ios/BleManager.mm
@@ -1,160 +1,157 @@
 #import "BleManager.h"
 
-@implementation BleManager
+#ifdef RCT_NEW_ARCH_ENABLED
 
-RCT_EXPORT_MODULE()
+@interface RCT_EXTERN_MODULE(BleManager, NativeBleManagerSpecBase <NativeBleManagerSpec>)
+@end
+
+#else
+
+@interface RCT_EXTERN_MODULE(BleManager, NSObject)
 
 RCT_EXTERN_METHOD(start:
-                  (NSDictionary *)options
-                  callback:(RCTResponseSenderBlock) callback)
+(NSDictionary *)options
+callback:(RCTResponseSenderBlock) callback)
 
 RCT_EXTERN_METHOD(scan:
-                  (NSArray *)serviceUUIDStrings
-                  timeoutSeconds:(nonnull NSNumber *)timeoutSeconds
-                  allowDuplicates:(BOOL)allowDuplicates
-                  scanningOptions:(nonnull NSDictionary*)scanningOptions
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSArray *)serviceUUIDStrings
+timeoutSeconds:(nonnull NSNumber *)timeoutSeconds
+allowDuplicates:(BOOL)allowDuplicates
+scanningOptions:(nonnull NSDictionary*)scanningOptions
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(stopScan:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(connect:
-                  (NSString *)peripheralUUID
-                  options:(NSDictionary *)options
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+options:(NSDictionary *)options
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(disconnect:
-                  (NSString *)peripheralUUID
-                  force:(BOOL)force
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+force:(BOOL)force
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(retrieveServices:
-                  (NSString *)peripheralUUID
-                  services:(NSArray<NSString *> *)services
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+services:(NSArray<NSString *> *)services
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(readRSSI:
-                  (NSString *)peripheralUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(readDescriptor:
-                  (NSString *)peripheralUUID
-                  serviceUUID:(NSString*)serviceUUID
-                  characteristicUUID:(NSString*)characteristicUUID
-                  descriptorUUID:(NSString*)descriptorUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+serviceUUID:(NSString*)serviceUUID
+characteristicUUID:(NSString*)characteristicUUID
+descriptorUUID:(NSString*)descriptorUUID
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(writeDescriptor:
-                  (NSString *)peripheralUUID
-                  serviceUUID:(NSString*)serviceUUID
-                  characteristicUUID:(NSString*)characteristicUUID
-                  descriptorUUID:(NSString*)descriptorUUID
-                  message:(NSArray*)message
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+serviceUUID:(NSString*)serviceUUID
+characteristicUUID:(NSString*)characteristicUUID
+descriptorUUID:(NSString*)descriptorUUID
+message:(NSArray*)message
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(getDiscoveredPeripherals:
-                  (nonnull RCTResponseSenderBlock)callback)
+(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(checkState:
-                  (nonnull RCTResponseSenderBlock)callback)
+(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(write:
-                  (NSString *)peripheralUUID
-                  serviceUUID:(NSString*)serviceUUID
-                  characteristicUUID:(NSString*)characteristicUUID
-                  message:(NSArray*)message
-                  maxByteSize:(NSInteger)maxByteSize
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+serviceUUID:(NSString*)serviceUUID
+characteristicUUID:(NSString*)characteristicUUID
+message:(NSArray*)message
+maxByteSize:(NSInteger)maxByteSize
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(writeWithoutResponse:
-                  (NSString *)peripheralUUID
-                  serviceUUID:(NSString*)serviceUUID
-                  characteristicUUID:(NSString*)characteristicUUID
-                  message:(NSArray*)message
-                  maxByteSize:(NSInteger)maxByteSize
-                  queueSleepTime:(NSInteger)queueSleepTime
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+serviceUUID:(NSString*)serviceUUID
+characteristicUUID:(NSString*)characteristicUUID
+message:(NSArray*)message
+maxByteSize:(NSInteger)maxByteSize
+queueSleepTime:(NSInteger)queueSleepTime
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(read:(NSString *)peripheralUUID
-                  serviceUUID:(NSString*)serviceUUID
-                  characteristicUUID:(NSString*)characteristicUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+serviceUUID:(NSString*)serviceUUID
+characteristicUUID:(NSString*)characteristicUUID
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(startNotification:
-                  (NSString *)peripheralUUID
-                  serviceUUID:(NSString*)serviceUUID
-                  characteristicUUID:(NSString*)characteristicUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+serviceUUID:(NSString*)serviceUUID
+characteristicUUID:(NSString*)characteristicUUID
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(stopNotification:
-                  (NSString *)peripheralUUID
-                  serviceUUID:(NSString*)serviceUUID
-                  characteristicUUID:(NSString*)characteristicUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+serviceUUID:(NSString*)serviceUUID
+characteristicUUID:(NSString*)characteristicUUID
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(getConnectedPeripherals:(NSArray *)serviceUUIDStrings
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(isPeripheralConnected:
-                  (NSString *)peripheralUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(isScanning:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(getMaximumWriteValueLengthForWithoutResponse:
-                  (NSString *)peripheralUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)peripheralUUID
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(getMaximumWriteValueLengthForWithResponse:
-                  (NSString *)deviceUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+(NSString *)deviceUUID
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(enableBluetooth:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(getBondedPeripherals:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(createBond:(NSString *)peripheralUUID
-                  devicePin:(NSString *)devicePin
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+devicePin:(NSString *)devicePin
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(removeBond:(NSString *)peripheralUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(removePeripheral:(NSString *)peripheralUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(requestMTU:(NSString *)peripheralUUID
-                  mtu:(NSInteger)mtu
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+mtu:(NSInteger)mtu
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(requestConnectionPriority:(NSString *)peripheralUUID
-                  connectionPriority:(NSInteger)connectionPriority
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+connectionPriority:(NSInteger)connectionPriority
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(refreshCache:(NSString *)peripheralUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(setName:(NSString *)name
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(getAssociatedPeripherals:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(removeAssociatedPeripheral:(NSString *)peripheralUUID
-                  callback:(nonnull RCTResponseSenderBlock)callback)
+callback:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(supportsCompanion:(nonnull RCTResponseSenderBlock)callback)
 
 RCT_EXTERN_METHOD(companionScan:
-                  (NSArray *)serviceUUIDs
-                  callback:(nonnull RCTResponseSenderBlock)callback)
-
-#ifdef RCT_NEW_ARCH_ENABLED
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-(const facebook::react::ObjCTurboModule::InitParams &)params
-{
-return std::make_shared<facebook::react::NativeBleManagerSpecJSI>(params);
-}
+(NSArray *)serviceUUIDs
+callback:(nonnull RCTResponseSenderBlock)callback)
+@end
 
 #endif
 
-@end
