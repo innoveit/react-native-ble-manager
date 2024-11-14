44a0e5deb92411e7d5ce7022d41cccdf31918921
Marco Sinigaglia <marco.sinigaglia@innove.it>
2024-11-14 16:38:31 +0100

    Fix import

--- ios/BleManager.h
+++ ios/BleManager.h
@@ -8,7 +8,7 @@ And enables old bridge just if user sets RCT_NEW_ARCH_ENABLED=0 on env.
 */
 
 #ifdef RCT_NEW_ARCH_ENABLED
-#import "RNBleManagerSpec.h"
+#import "BleManagerSpec/BleManagerSpec.h"
 
 #else
 #import <React/RCTBridgeModule.h>
