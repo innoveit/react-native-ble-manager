41dfbe399a5e43f455b8248fda9b5dc43d07e283
td-miloszdubiel <milosz@jrdltd.co.uk>
2025-08-29 07:38:26 +0200

    [iOS] Add `NotifyBufferContainer`

---
+++ ios/NotifyBufferContainer.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+class NotifyBufferContainer {
+    public var items: Data
+    public let capacity: Int
+    
+    public var count: Int {
+        items.count
+    }
+    
+    public var remaining: Int {
+        capacity - items.count
+    }
+    
+    public var isBufferFull: Bool {
+        items.count >= capacity
+    }
+    
+    init(size: Int) {
+        self.capacity = size
+        self.items = Data(capacity: size)
+    }
+    
+    init(size: NSNumber) {
+        self.capacity = size.intValue
+        self.items = Data(capacity: size.intValue)
+    }
+    
+    public func resetBuffer() {
+        items.removeAll(keepingCapacity: true)
+    }
+    
+    public func put(_ value: Data) -> Data {
+        let remainingCapacity = self.remaining
+        let restLength = value.count - remainingCapacity
+        
+        let toInsert: Data
+        let rest: Data
+        if restLength > 0 {
+            toInsert = value.prefix(remainingCapacity)
+            rest = value.suffix(restLength)
+        }
+        else {
+            toInsert = value
+            rest = Data()
+        }
+        items.append(toInsert)
+        
+        return rest
+    }
+}
